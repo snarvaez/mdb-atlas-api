7a5319d5694db4fe1cd2ffbe3d6477ef31acd222
Sig Narváez <sigfrido@mongodb.com>
2019-10-02 12:30:03 -0700

    Update cluster-upscale-sharded.py

--- cluster-modify/cluster-upscale-sharded.py
+++ cluster-modify/cluster-upscale-sharded.py
@@ -11,8 +11,8 @@ import pycurl
 import requests
 from requests.auth import HTTPDigestAuth
 
-userName    = "ATLAS-USER-NAME"
-apiKey      = "ATLAS-API-KEY"
+apiPublicKey  = "ATLAS-API-PUBLIC-KEY"
+apiPrivateKey = "ATLAS-API-PRIVATE-KEY"
 projectId   = "ATLAS-PROJECTID"
 clusterName = "SA-PoV"
 
@@ -47,20 +47,25 @@ data = {
 t1 = datetime.datetime.now()
 print(t1, " - Creating cluster: ", clusterName)
 response = requests.patch(urlModify, headers=headers, data=json.dumps(data),
-                         auth=HTTPDigestAuth(userName, apiKey))
+                        auth=HTTPDigestAuth(apiPublicKey, apiPrivateKey))
 
-timeout = time.time() + 60*15  # 15 minute timeout
-while True:
-    response = requests.get(urlStatus, auth=HTTPDigestAuth(userName, apiKey))
-    stateName = response.json()["stateName"]
-    t2 = datetime.datetime.now()
-    print(t2, " - Cluster status: ", stateName)
+print(response)
+if response.status_code != 200:
+   print('Error:')
+   print(response.json())
+else:
+	timeout = time.time() + 60*15  # 15 minute timeout
+	while True:
+		response = requests.get(urlStatus, auth=HTTPDigestAuth(apiPublicKey, apiPrivateKey))
+		stateName = response.json()["stateName"]
+		t2 = datetime.datetime.now()
+		print(t2, " - Cluster status: ", stateName)
 
-    if stateName == "IDLE":
-        print("Cluster created in: ", t2-t1)
-        break
-    if time.time() > timeout:
-        print("TIMEOUT: Cluster creation is still in progress")
-        print("Please go to the MongoDB Atlas UI.")
-        break
-    time.sleep(5)
+		if stateName == "IDLE":
+			print("Cluster created in: ", t2-t1)
+			break
+		if time.time() > timeout:
+			print("TIMEOUT: Cluster modification is still in progress")
+			print("Please go to the MongoDB Atlas UI.")
+			break
+		time.sleep(5)
