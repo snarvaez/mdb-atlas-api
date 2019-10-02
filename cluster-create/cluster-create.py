eb6c7da2d972d713575f4ffaf8040376a45cb424
Sig Narváez <sigfrido@mongodb.com>
2019-10-02 12:25:14 -0700

    Update to M10 and use api public and private key

--- cluster-create/cluster-create.py
+++ cluster-create/cluster-create.py
@@ -6,13 +6,15 @@
 
 # REPLACE your Atlas Username, API Key and ProjectId below
 
-import json, sys, time, datetime
-import pycurl
+import json
+import sys
+import time
+import datetime
 import requests
 from requests.auth import HTTPDigestAuth
 
-userName    = "ATLAS-USER-NAME"
-apiKey      = "ATLAS-API-KEY"
+apiPublicKey  = "ATLAS-API-PUBLIC-KEY"
+apiPrivateKey = "ATLAS-API-PRIVATE-KEY"
 projectId   = "ATLAS-PROJECTID"
 clusterName = "SA-PoV"
 
@@ -36,7 +38,7 @@ data = {
   "providerSettings": {
     "providerName": "AWS",
     "regionName": "US_WEST_2",
-    "instanceSizeName": "M30",
+    "instanceSizeName": "M10",
     "encryptEBSVolume": True
   },
   "replicationFactor": 3,
@@ -47,20 +49,23 @@ data = {
 t1 = datetime.datetime.now()
 print(t1, " - Creating cluster: ", clusterName)
 response = requests.post(urlCreate, headers=headers, data=json.dumps(data),
-                         auth=HTTPDigestAuth(userName, apiKey))
+                        auth=HTTPDigestAuth(apiPublicKey, apiPrivateKey))
+if response.status_code != 201:
+   print('Error:')
+   print(response.json())
+else:
+	timeout = time.time() + 60*15  # 15 minute timeout
+	while True:
+		response = requests.get(urlStatus, auth=HTTPDigestAuth(apiPublicKey, apiPrivateKey))
+		stateName = response.json()["stateName"]
+		t2 = datetime.datetime.now()
+		print(t2, " - Cluster status: ", stateName)
 
-timeout = time.time() + 60*15  # 15 minute timeout
-while True:
-    response = requests.get(urlStatus, auth=HTTPDigestAuth(userName, apiKey))
-    stateName = response.json()["stateName"]
-    t2 = datetime.datetime.now()
-    print(t2, " - Cluster status: ", stateName)
-
-    if stateName == "IDLE":
-        print("Cluster created in: ", t2-t1)
-        break
-    if time.time() > timeout:
-        print("TIMEOUT: Cluster creation is still in progress")
-        print("Please go to the MongoDB Atlas UI.")
-        break
-    time.sleep(5)
+		if stateName == "IDLE":
+			print("Cluster created in: ", t2-t1)
+			break
+		if time.time() > timeout:
+			print("TIMEOUT: Cluster creation is still in progress")
+			print("Please go to the MongoDB Atlas UI.")
+			break
+		time.sleep(5)
